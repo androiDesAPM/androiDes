27ce6d06a9a7143e7f75ea1e205e009dac3b0f87
mariarescobar <maria.rescobar@udc.es>
2023-03-21 22:58:27 +0100

    actividad detalles de concierto y detalles de artista conciertos pasados

---
+++ app/src/main/java/com/apm/apm/ArtistDetailsPastActivity.kt
@@ -0,0 +1,11 @@
+package com.apm.apm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ArtistDetailsPastActivity  : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.artist_details_past)
+    }
+}
---
+++ app/src/main/java/com/apm/apm/ConcertDetailsActivity.kt
@@ -0,0 +1,10 @@
+package com.apm.apm
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConcertDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.artist_concert)
+    }
+}
