36f670529705f4342f9bdf82968b18201ad0cefc
Antón Cao <anton.cao@udc.es>
2023-03-23 02:23:18 +0100

    Uso de buscador del mapa para generar un toast. Posible esqueleto para sugerencias en el texto

--- app/src/main/java/com/apm/apm/MapActivity.kt
+++ app/src/main/java/com/apm/apm/MapActivity.kt
@@ -2,6 +2,9 @@ package com.apm.apm
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -11,17 +14,24 @@ class MapActivity : GetNavigationBarActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.map)
 
+        val searchView = findViewById<SearchView>(R.id.searchViewOnMap)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MapActivity, "Busqueda del artista $query", Toast.LENGTH_LONG).show()
+                return true
+            }
+
+            //TODO borrar esto si al final no metemos sugerencias
+            override fun onQueryTextChange(newText: String): Boolean {
+                // Escribir aqui el código de las sugerencias para cuando el usuario este escribiendo
+                // Habría que poner algo como app:querySuggestionEnabled="true" en el xml
+                return false
+            }
+        })
+
+
         //Creamos la barra inferior
         this.getNavigationView()
 
-
-
-
-        //TODO igual meter que aparezca algo al updatear el buscador aunque el mapa no se vea todavía
-//        mActivityName = getString(R.string.activity_b_label)
-//        mStatusView = findViewById<View>(R.id.status_view_b) as TextView
-//        mStatusAllView = findViewById<View>(R.id.status_view_all_b) as TextView
-//        mStatusTracker.setStatus(mActivityName!!, getString(R.string.on_create))
-//        printStatus(mStatusView, mStatusAllView)
     }
 }
