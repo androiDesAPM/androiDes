8136f68c560a3d9f8627a03fb8931f8dd12be5fc
mariarescobar <maria.rescobar@udc.es>
2023-04-11 20:11:49 +0200

    Añadidos xmls de los fragments que muestran las listas de conciertos

---
+++ app/src/main/java/com/apm/apm/FutureConcertsFragment.kt
@@ -0,0 +1,13 @@
+package com.apm.apm
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
+
+class FutureConcertsFragment : Fragment() {
+}
