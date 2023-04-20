ffe19095d45a644cca64a1994bbb9ba8886e113d
jsilvcast <josesilvacastro1994@gmail.com>
2023-04-20 19:36:28 +0200

    Added auth activity and kotlin class

---
+++ app/src/main/java/com/apm/apm/AuthActivity.kt
@@ -0,0 +1,11 @@
+package com.apm.apm
+
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+
+}
