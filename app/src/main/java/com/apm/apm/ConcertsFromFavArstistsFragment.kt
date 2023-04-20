0029ed9cf6be05479e779c037497a9f12155e595
mariarescobar <maria.rescobar@udc.es>
2023-04-20 18:43:41 +0200

    Modificada corrutina para que use Lifecycle

--- app/src/main/java/com/apm/apm/ConcertsFromFavArstistsFragment.kt
+++ app/src/main/java/com/apm/apm/ConcertsFromFavArstistsFragment.kt
@@ -1,29 +1,31 @@
 package com.apm.apm
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.apm.apm.adapter.FavArtistAdapter
 import com.apm.apm.objects.Concert
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import java.time.LocalDate
+import kotlinx.coroutines.delay
 
-class ConcertsFromFavArstistsFragment : Fragment() {
+class ConcertsFromFavArstistsFragment : Fragment(), LifecycleOwner {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: FavArtistAdapter
-
-//    val concerts = listOf(
-//        Concert("Lugar concierto", LocalDate.now(),"nombreArtista"),
-//        Concert("Lugar concierto", LocalDate.now(),"nombreArtista"),
-//        Concert("Lugar concierto", LocalDate.now(),"nombreArtista")
-//    )
+    //Almacena una referencia al job de la corrutina
+    private lateinit var job: Job
     val concerts = mutableListOf<Concert>()
 
     override fun onCreateView(
@@ -54,41 +56,30 @@ class ConcertsFromFavArstistsFragment : Fragment() {
         val progressBar: ProgressBar = view.findViewById(R.id.progressbar)
         progressBar.visibility = View.VISIBLE
 
-        concerts.add(Concert("Lugar concierto", LocalDate.now(),"Rosalía"))
-        concerts.add(Concert("Lugar concierto2", LocalDate.now(),"Bad Gyal"))
-        concerts.add(Concert("Lugar concierto3", LocalDate.now(),"nombreArtista3"))
-        concerts.add(Concert("Lugar concierto4", LocalDate.now(),"nombreArtista4"))
-        concerts.add(Concert("Lugar concierto5", LocalDate.now(),"nombreArtista5"))
+        concerts.add(Concert("Lugar concierto", LocalDate.now(), "Rosalía"))
+        concerts.add(Concert("Lugar concierto2", LocalDate.now(), "Bad Gyal"))
+        concerts.add(Concert("Lugar concierto3", LocalDate.now(), "nombreArtista3"))
+        concerts.add(Concert("Lugar concierto4", LocalDate.now(), "nombreArtista4"))
+        concerts.add(Concert("Lugar concierto5", LocalDate.now(), "nombreArtista5"))
 
-        //TODO descomentar para usar la corrutina
-//        getConcertsCorrutine(progressBar)
+        lifecycleScope.launch {
+            getConcertsCorrutine(progressBar)
 
-//        val lv = view.findViewById<ListView>(R.id.listConcertsFavArtistsRecycleView)
-//        recyclerView.setOnItemClickListener { _, _, position, _ ->
-//            val intent = Intent(requireContext(), ArtistDetailsActivity::class.java)
-//            intent.putExtra("selected_item", concerts[position])
-//            startActivity(intent)
-//        }
-
-//        val layoutManager =
-//            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-//
-//        val myList = view.findViewById(R.id.listConcertsFavArtistsRecycleView) as RecyclerView
-//        myList.layoutManager = layoutManager
+        }
     }
 
     private fun getConcertsCorrutine(progressBar: ProgressBar) {
-        GlobalScope.launch { // créase unha nova corrutina en segundo plano
-//            delay(2000L) // delay non bloqueante (do thread actual) de 1000 milisegundos
+        job = lifecycleScope.launch {
+            delay(2000L) // delay non bloqueante (do thread actual) de 1000 milisegundos
 
             //Petición a la API
 
             //Añadir los conciertos encontrados
-            concerts.add(Concert("Lugar concierto", LocalDate.now(),"Rosalía"))
-            concerts.add(Concert("Lugar concierto2", LocalDate.now(),"Bad Gyal"))
-            concerts.add(Concert("Lugar concierto3", LocalDate.now(),"nombreArtista3"))
-            concerts.add(Concert("Lugar concierto4", LocalDate.now(),"nombreArtista4"))
-            concerts.add(Concert("Lugar concierto5", LocalDate.now(),"nombreArtista5"))
+            concerts.add(Concert("Lugar concierto", LocalDate.now(), "Rosalía"))
+            concerts.add(Concert("Lugar concierto2", LocalDate.now(), "Bad Gyal"))
+            concerts.add(Concert("Lugar concierto3", LocalDate.now(), "nombreArtista3"))
+            concerts.add(Concert("Lugar concierto4", LocalDate.now(), "nombreArtista4"))
+            concerts.add(Concert("Lugar concierto5", LocalDate.now(), "nombreArtista5"))
 
             progressBar.visibility = View.INVISIBLE
         }
@@ -97,5 +88,12 @@ class ConcertsFromFavArstistsFragment : Fragment() {
 
         println("Cargando conciertos ....") // o thread principal continúa durante o delay da corutina
         Thread.sleep(5000L) // bloquéase o thread actual durante dous segundos
+    }
+
+
+    override fun onDestroy() {
+        //cancela la corrutina
+        super.onDestroy()
+        job.cancel()
     }
 }
