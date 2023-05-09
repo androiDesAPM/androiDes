5bf0cc8596812c3bb39d938397e9175cc39b62fa
mariarescobar <maria.rescobar@udc.es>
2023-05-09 18:47:56 +0200

    Implemetado ver detalles de concierto (no esta acabado)

--- app/src/main/java/com/apm/apm/ConcertDetailsActivity.kt
+++ app/src/main/java/com/apm/apm/ConcertDetailsActivity.kt
@@ -2,12 +2,25 @@ package com.apm.apm
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import com.apm.apm.objects.Artist
+import com.apm.apm.objects.Concert
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.launch
 
 class ConcertDetailsActivity : GetNavigationBarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.artist_concert)
 
+        val concert: Concert? = intent.getParcelableExtra("concert")
+        lifecycleScope.launch {
+            if (concert != null){
+                showConcertDetails(concert)
+            }
+        }
         val concertButton = findViewById<Button>(R.id.buy_tickets)
         concertButton.setOnClickListener {
             val intent = Intent(this, BuyTicketsActivity::class.java)
@@ -16,6 +29,29 @@ class ConcertDetailsActivity : GetNavigationBarActivity() {
 
         //Creamos la barra inferior
         this.getNavigationView()
+
+    }
+
+
+    private fun showConcertDetails(concert: Concert) {
+        val artistImageView = findViewById<ImageView>(R.id.imageView3)
+        val artistNameTextView = findViewById<TextView>(R.id.textView10)
+        val concertLocation = findViewById<TextView>(R.id.textView11)
+        val concertDate = findViewById<TextView>(R.id.textView12)
+        //estos faltan
+        val concertStreet = findViewById<TextView>(R.id.textView13)
+        val concertVenue = findViewById<TextView>(R.id.textView14)
+        //precio iria aqui tambien?
+
+        artistNameTextView.text = concert.concertArtistName
+        concertLocation.text = concert.concertLocationName
+        concertDate.text = concert.concertDate.toString()
+
+        Picasso.get()
+            .load(concert.imageUrl)
+            .fit()
+            .centerCrop()
+            .into(artistImageView)
 
     }
 }
--- app/src/main/java/com/apm/apm/objects/Concert.kt
+++ app/src/main/java/com/apm/apm/objects/Concert.kt
@@ -1,10 +1,42 @@
 package com.apm.apm.objects
 
-import android.graphics.Bitmap
+import android.os.Parcel
+import android.os.Parcelable
 import java.time.LocalDate
+import java.util.*
 
-class Concert (val concertLocationName: String,
-               val concertDate: LocalDate,
-               val concertArtistName: String,
-               val imageUrl: String? = null){
+class Concert(
+    val concertLocationName: String,
+    val concertDate: LocalDate,
+    val concertArtistName: String,
+    val imageUrl: String? = null
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        LocalDate.ofEpochDay(parcel.readLong()),
+        parcel.readString() ?: "",
+        parcel.readString() ?: null
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(concertLocationName)
+        parcel.writeLong(concertDate.toEpochDay())
+        parcel.writeString(concertArtistName)
+        parcel.writeString(imageUrl)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Concert> {
+        override fun createFromParcel(parcel: Parcel): Concert {
+            return Concert(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Concert?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
