684b4e75551ccc3a9854fef6cb725ca9bd67f84b
jsilvcast <josesilvacastro1994@gmail.com>
2023-05-25 01:37:11 +0200

    Added user city by places api. Updated xml and check if all fields are correctly filed. Update user document db with location information

--- app/src/main/java/com/apm/apm/RegisterActivity.kt
+++ app/src/main/java/com/apm/apm/RegisterActivity.kt
@@ -4,9 +4,17 @@ import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -15,10 +23,43 @@ import com.google.firebase.ktx.Firebase
 
 
 class RegisterActivity : AppCompatActivity() {
-
+    var latLng: com.google.android.gms.maps.model.LatLng? = null
+    var city: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val apiKey = BuildConfig.PLACES_API_KEY
+
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, apiKey)
+        }
+
+        // Initialize the AutocompleteSupportFragment.
+        val autocompleteFragment = supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment?
+
+        autocompleteFragment?.setHint("City")
+
+        // Specify the types of place data to return.
+        autocompleteFragment!!.setPlaceFields(
+            listOf(
+                Place.Field.ID,
+                Place.Field.NAME,
+                Place.Field.LAT_LNG
+            )
+        )
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment?.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                latLng = place.latLng
+                city = place.name
+            }
+
+            override fun onError(status: Status) {
+                Log.i("place", "An error occurred: $status")
+            }
+        })
+
         setup()
     }
 
@@ -31,10 +72,16 @@ class RegisterActivity : AppCompatActivity() {
         val loginHere = findViewById<ConstraintLayout>(R.id.registerLoginHere)
 
         registerButton.setOnClickListener {
-            if (fullName.text.isNotEmpty() && email.text.isNotEmpty() && password.text.isNotEmpty() && passwordRep.text.isNotEmpty()){
+            if (fullName.text.isNotEmpty() && email.text.isNotEmpty() && password.text.isNotEmpty() && passwordRep.text.isNotEmpty() && city.isNotEmpty()){
                 if (password.text.toString() == passwordRep.text.toString()){
                     registerUser(email.text.toString(), password.text.toString(), fullName.text.toString())
                 }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Please fill all the fields",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
@@ -70,11 +117,17 @@ class RegisterActivity : AppCompatActivity() {
         val db = Firebase.firestore
         val username = "User" + uid?.substring(0, 3)
         val coins = 1000
+        val latLonDB = hashMapOf(
+            "lat" to latLng?.latitude,
+            "lon" to latLng?.longitude,
+            "city" to city
+        )
         val userToUpload = hashMapOf(
             "username" to username,
             "email" to email,
             "fullName" to fullName,
-            "coins" to coins
+            "coins" to coins,
+            "location" to latLonDB,
         )
         db.collection("users").document(uid ?: "").set(userToUpload)
             .addOnSuccessListener { documentReference ->
