5181919e67a61c84bae47a20cda271ae7367725c
Antón Cao <anton.cao@udc.es>
2023-05-25 07:37:24 +0200

    La actividad se conecta correctamente con el storage para descargar una imagen e insertarla en un ImageView, también se puede subir una imagen correctamente

--- app/src/main/java/com/apm/apm/ArtistPhotosActivity.kt
+++ app/src/main/java/com/apm/apm/ArtistPhotosActivity.kt
@@ -3,6 +3,7 @@ package com.apm.apm
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.OpenableColumns
@@ -10,7 +11,6 @@ import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -24,15 +24,16 @@ import com.google.firebase.storage.ktx.storage
 class ArtistPhotosActivity: GetNavigationBarActivity() {
     private lateinit var adapter: ArtistPhotosAdapter
     private val storage = Firebase.storage
+    private var artistId = 0
+    val imagesList: MutableList<Bitmap> = mutableListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.artist_photos)
-        val artistId = intent.getStringExtra("artistId")
+        //val artistId = intent.getStringExtra("artistId")
+        this.artistId = 123
         val db = Firebase.firestore
-
-        val artistRef = artistId?.let { db.collection("artists").document(it) }
-
         val recyclerView = findViewById<RecyclerView>(R.id.artist_photos_row)
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
@@ -42,14 +43,6 @@ class ArtistPhotosActivity: GetNavigationBarActivity() {
 
         //Funcionalidad para subir una imagen
         val uploadPhotoButton = findViewById<ImageButton>(R.id.uploadPhotoButton)
-//        uploadPhotoButton.setOnClickListener {
-//            // Aquí colocar el código que deseas ejecutar cuando se hace clic en el botón
-//            // Por ejemplo, puedes iniciar la selección de la imagen aquí
-//            val galleryIntent = Intent(Intent.ACTION_PICK)
-//            galleryIntent.type = "image/*"
-//            imagePickerActivityResult.launch(galleryIntent)
-//        }
-
         uploadPhotoButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 val galleryIntent = Intent(Intent.ACTION_PICK)
@@ -57,78 +50,159 @@ class ArtistPhotosActivity: GetNavigationBarActivity() {
                 imagePickerActivityResult.launch(galleryIntent)
             }
         })
+        val artistsRef2 = artistId?.let { storage.reference.child("artists/$artistId")}
+        if (artistsRef2 != null) {
+            val listAll = artistsRef2.listAll()
 
-
-        artistRef?.get()?.addOnSuccessListener { document ->
-            if (document.exists()) {
-                val artistData = document.data
-                val imageUrls = artistData?.get("images") as? List<String>
-
-                if (imageUrls != null && imageUrls.isNotEmpty()) {
-                    // La lista de imágenes no está vacía
-                    adapter.updateData(imageUrls)
-                    // Configurar el RecyclerView con el ArtistPhotoAdapter y la lista de imágenes
-                   // val recyclerView = findViewById<RecyclerView>(R.id.artist_photos_row)
-                    7//val adapter = ArtistPhotosAdapter(this, imageUrls)
-                    //recyclerView.adapter = adapter
-                } else {
-                    // La lista de imágenes está vacía
-                    Toast.makeText(applicationContext, "No se han añadido imágenes todavía", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                // El documento del artista no existe, se crea
-                val artistData = HashMap<String, Any>() // Agrega los campos necesarios
-                db.collection("artists").document(artistId)
-                    .set(artistData)
-                    .addOnSuccessListener {
-                        // El documento del artista se creó exitosamente
-                    }
-                    .addOnFailureListener { exception ->
-                        // Ocurrió un error al crear el documento del artista
-                        println("Error al crear el documento del artista: $exception")
-                    }
-            }
-        }?.addOnFailureListener { exception ->
-            // Error al obtener los datos del artista desde Firestore
-            Toast.makeText(applicationContext, "Error al obtener los datos del artista", Toast.LENGTH_SHORT).show()
         }
 
 
+        /****************************La edición buena*************************************/
 
-//         uploadPhotoButton.setOnClickListener {
-//            // PICK INTENT picks item from data
-//            // and returned selected item
-//            val galleryIntent = Intent(Intent.ACTION_PICK)
-//            // here item is type of image
-//            galleryIntent.type = "image/*"
-//            // ActivityResultLauncher callback
-//            imagePickerActivityResult.launch(galleryIntent)
+        val storageRef1 = storage.reference
+        val artistsRef1 = storageRef1.child("artists/123")
+
+        artistsRef1.list(12 * 1)
+            .addOnSuccessListener { listResult ->
+                val items = listResult.items
+                var stringList: ArrayList<String> = ArrayList()
+                if (items.isNotEmpty()) { //ES ESTE ES ESTE
+                    val imagePromises = items.take(12).map { item ->
+//                        val downloadUrl = item.downloadUrl.toString()
+                        val artistImageView = findViewById<ImageView>(R.id.imageView)
+                        var download1 = item.downloadUrl.addOnSuccessListener{
+                            Glide.with(this@ArtistPhotosActivity)
+                                .load(it)
+                                .into(artistImageView)
+
+                            Log.e("Firebase", "download passed")
+                        }
+                        /**************************************Con este cacho funciona********************************************/
+//                        val artistImageView = findViewById<ImageView>(R.id.imageView)
+//                        var download1 = storage.reference.child("artists/123/a-brain-riding-a-rocketship.jpg").downloadUrl.addOnSuccessListener{
+//                            Glide.with(this@ArtistPhotosActivity)
+//                                .load(it)
+//                                .into(artistImageView)
+//
+//                            Log.e("Firebase", "download passed")
+//                        }
+                        /********************************************************************************************************/
+
+//                        val downloadUrl = item.downloadUrl.addOnSuccessListener{
+//                            Glide.with(this@ArtistPhotosActivity)
+//                                .load(it)
+//                                .into(artistImageView)
+//
+//                            Log.e("Firebase", "download passed")
+//                        }.addOnFailureListener {
+//                            Log.e("Firebase", "Failed in downloading")
+//                        }
+//                        val downloadUrl = item.toString()
+//                        stringList.add(downloadUrl)
+//                        adapter.updateData(stringList)
+//                        val imageRef = storageRef1.child(item.name)
+//                        imageRef.getBytes(ONE_MEGABYTE.toLong()).asDeferred()
+                    }
+//                    CoroutineScope(Dispatchers.IO).launch {
+////                        val bitmaps = imagePromises.awaitAll().mapNotNull { bytes ->
+////                            BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+//                        }
+////                        withContext(Dispatchers.Main) {
+////                            imagesList.addAll(bitmaps)
+////                            recyclerView.adapter?.notifyDataSetChanged()
+//                        }
+//                    }
+                }
+            }
+
+        /****************************La manera que parece que funciona*************************************/
+
+//        val storageRef = storage.reference
+//        val artistsRef = storageRef.child("artists/123")
+//
+//        artistsRef.list(12 * 1)
+//            .addOnSuccessListener { listResult ->
+//                val items = listResult.items
+//                if (items.isNotEmpty()) { //ES ESTE ES ESTE
+//                    val imagePromises = items.take(12).map { item ->
+//                        val imageRef = artistsRef.child(item.name)
+//                        imageRef.getBytes(ONE_MEGABYTE.toLong()).asDeferred()
+//                    }
+//                    CoroutineScope(Dispatchers.IO).launch {
+//                        val bitmaps = imagePromises.awaitAll().mapNotNull { bytes ->
+//                            BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+//                        }
+//                        withContext(Dispatchers.Main) {
+//                            imagesList.addAll(bitmaps)
+//                            recyclerView.adapter?.notifyDataSetChanged()
+//                        }
+//                    }
+//                }
+//            }
+
+
+        /****************************Otra manera*************************************/
+
+//        val artistRef = artistId?.let { storage.reference.child("artists/$artistId") }
+//
+//        artistRef?.listAll()?.addOnSuccessListener { result ->
+//            val imageUrls = result.items.map { it.downloadUrl.toString() }
+//            val downloadUrl2 = result.items.get(0).downloadUrl
+//            if (imageUrls.isNotEmpty()) {
+//                // La lista de imágenes no está vacía
+//                adapter.updateData(imageUrls)
+//                // Configurar el RecyclerView con el ArtistPhotoAdapter y la lista de imágenes
+//               // val recyclerView = findViewById<RecyclerView>(R.id.artist_photos_row)
+//                //val adapter = ArtistPhotosAdapter(this, imageUrls)
+//                //recyclerView.adapter = adapter
+//            } else {
+//                // La lista de imágenes está vacía
+//                Toast.makeText(applicationContext, "No se han añadido imágenes todavía", Toast.LENGTH_SHORT).show()
+//            }
+////            result.items.get(0).listAll().result.items.get(0).downloadUrl //TODO borrar esta linea
+//
+//
+//        }?.addOnFailureListener { exception ->
+//            // Error al obtener los datos del artista desde Firestore
+//            Toast.makeText(applicationContext, "Error al obtener los datos del artista", Toast.LENGTH_SHORT).show()
 //        }
 
-        //Creamos la barra inferior
+        /****************************Otra manera*************************************/
+
+//        storage.reference.child("artists/$artistId" +"123").getDownloadUrl().addOnSuccessListener{ result ->
+//            val imageUrls = result.toString()
+//            val result2 = result//TODO borrar esto
+//            if (imageUrls.isNotEmpty()) {
+//                // La lista de imágenes no está vacía
+////                adapter.updateData(imageUrls)
+//                // Configurar el RecyclerView con el ArtistPhotoAdapter y la lista de imágenes
+//                // val recyclerView = findViewById<RecyclerView>(R.id.artist_photos_row)
+//                //val adapter = ArtistPhotosAdapter(this, imageUrls)
+//                //recyclerView.adapter = adapter
+//            } else {
+//                // La lista de imágenes está vacía
+//                Toast.makeText(applicationContext, "No se han añadido imágenes todavía", Toast.LENGTH_SHORT).show()
+//            }
+//
+//        }?.addOnFailureListener { exception ->
+//            // Error al obtener los datos del artista desde Firestore
+//            Toast.makeText(applicationContext, "Error al obtener los datos del artista", Toast.LENGTH_SHORT).show()
+//        }
+
         this.getNavigationView()
 
     }
 
 
 
-    // Método para manejar el resultado de la selección de la imagen
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//
-//        super.onActivityResult(requestCode, resultCode, data)
-//
-//        if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK) {
-//            val selectedImageUri = data?.data
-//
-//            if (selectedImageUri != null) {
-//                // Aquí acciones con la imagen seleccionada, como subirla a Firebase Storage
-//                // Obtener la ruta de la imagen usando selectedImageUri.path
-//
-//                Toast.makeText(applicationContext, "Ruta de la imagen: ${selectedImageUri.path}", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    }
 
+    /****************************Subida de la imagen*************************************/
+
+
+
+
+
+    //Con esta funcion subimos la imagen
     private var imagePickerActivityResult: ActivityResultLauncher<Intent> =
     // lambda expression to receive a result back, here we
         // receive single item(photo) on selection
@@ -147,12 +221,12 @@ class ArtistPhotosActivity: GetNavigationBarActivity() {
 
                 // Upload Task with upload to directory 'file'
                 // and name of the file remains same
-                val uploadTask = storage.reference.child("file/$sd").putFile(imageUri)
+                val uploadTask = storage.reference.child("artists/$artistId/$sd").putFile(imageUri)
 
                 // On success, download the file URL and display it
                 uploadTask.addOnSuccessListener {
                     // using glide library to display the image
-                    storage.reference.child("upload/$sd").downloadUrl.addOnSuccessListener {
+                    storage.reference.child("artists/$artistId/$sd").downloadUrl.addOnSuccessListener {
                         Glide.with(this@ArtistPhotosActivity)
                             .load(it)
                             .into(artistImageView)
@@ -185,6 +259,8 @@ class ArtistPhotosActivity: GetNavigationBarActivity() {
 
     companion object {
         private const val REQUEST_IMAGE_PICK = 1
+        private const val ONE_MEGABYTE = 1_048_576 // 1 megabyte en bytes
+
     }
 
 
